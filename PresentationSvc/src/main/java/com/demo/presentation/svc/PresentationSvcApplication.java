1332cc637c8406a149af4fc2f22100f2908a717b
pal-bhaskar <69039695+pal-bhaskar@users.noreply.github.com>
2021-12-15 18:42:53 +0530

    correcting dependencies

--- PresentationSvc/src/main/java/com/demo/presentation/svc/PresentationSvcApplication.java
+++ PresentationSvc/src/main/java/com/demo/presentation/svc/PresentationSvcApplication.java
@@ -1,7 +1,9 @@
 package com.demo.presentation.svc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -9,6 +11,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PresentationSvcApplication {
+	
+	@Autowired
+	DiscoveryClient discoveryClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PresentationSvcApplication.class, args);
